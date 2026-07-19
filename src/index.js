d1d624cd0a85cb8c388166d3349145e4a009eed0
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2026-07-19 20:57:06 +0600

    session updated

--- src/index.js
+++ src/index.js
@@ -9,6 +9,30 @@ dotenv.config(); // fetches data frm dotenv file
 const app = express();
 
 //middlewares
+const corsOptions = {
+    origin:["http://localhost:3001"],
+    Credentials: true,
+}
 
+app.use(cors(corsOptions));
 app.use(json({limit:"100mb"}));
 app.use(urlencoded({limit:"100mb", extended: true}));
+app.use(session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000 * 60,
+    },
+})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+//Routes
+
+
+//Listem app
+const PORT = process.env.PORT || 7002;
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+})
