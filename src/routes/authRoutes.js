c8506752bb9693c88114d14645f7aa6ee075d798
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2026-07-23 10:40:39 +0600

    finished the setup of 2fa

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post("/register", register);
 
 // Login Route
-router.post("/login", login);
+router.post("/login", passport.authenticate("local") ,login);
 
 // Auth Status Route
 router.get("/status", authStatus);
@@ -17,12 +17,21 @@ router.get("/status", authStatus);
 router.post("/logout", logout);
 
 // 2FA setup
-router.post("/2fa/setup", setup2FA);
+router.post("/2fa/setup",(req, res, next)=> {
+    if(req.isAuthenticated()) return next();
+    res.status(401).json({message: "unauthorized"});
+} ,setup2FA);
 
 // Verify Route
-router.post("/2fa/verify", verify2FA);
+router.post("/2fa/verify",(req, res, next)=> {
+    if(req.isAuthenticated()) return next();
+    res.status(401).json({message: "unauthorized"});
+} , verify2FA);
 
 // Reset Route
-router.post("/2fa/reset", reset2FA);
+router.post("/2fa/reset",(req, res, next)=> {
+    if(req.isAuthenticated()) return next();
+    res.status(401).json({message: "unauthorized"});
+} , reset2FA);
 
 export default router;
