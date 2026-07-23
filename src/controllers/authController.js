f313b121556eb7212aa45f0ada02d730bf533c53
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2026-07-23 12:44:10 +0600

    backend is complete

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -77,5 +77,32 @@ export const setup2FA = async (req,res) => {
     }
 };
 
-export const verify2FA = async (req,res) => {};
-export const reset2FA = async (req,res) => {};
+export const verify2FA = async (req,res) => {
+    const {token} = req.body;
+    const user = req.user;
+
+    const verified = speakeasy.totp.verify({
+        secret: user.twoFactorSecret,
+        encoding: "base32",
+        token
+    });
+
+    if(verified) {
+        const jwtToken = jwt.sign({username: user.username}, process.env.JWT_SECRET,{expiresIn: "1hr"});
+        res.status(200).json({message: "2fa successfull", token: jwtToken,});
+    } else {
+        res.status(400).json({message: "invalid 2fa token"})
+    }
+};
+
+export const reset2FA = async (req,res) => {
+    try {
+        const user = req.user;
+        user.twoFactorSecret = "";
+        user.isMfaActive = false;
+        await user.save();
+        res.status(200).json({message: "2fa reset successfull"});
+    } catch (error) {
+        res.status(500).json({error: "error reseting 2fa", message: error});
+    }
+};
