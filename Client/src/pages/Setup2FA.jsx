acd81551701f891cd2b3419a9fd115f4b2f64b87
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2026-08-12 20:05:13 +0600

    ready to connect apis

---
+++ Client/src/pages/Setup2FA.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Setup2FA = () => {
+    <div>Setup2FA</div>;
+};
+
+
+export default Setup2FA;
---
+++ Client/src/pages/Verify2FA.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Verify2FA = () => {
+    <div>Verify2FA</div>;
+};
+
+
+export default Verify2FA;
