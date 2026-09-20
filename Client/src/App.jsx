a12fab36f82e137ac7d40b251272b07cd711cf99
Mr-Shajid <mdshahriarchyshajid.cse@gmail.com>
2026-09-21 03:54:50 +0600

    fixed some backend issues

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from "./routes"
 function App() {
   return (
-    <div className='bg-slate-900 h-screen'>
+    <div className='bg-slate-800 h-screen'>
       <div className='flex justify-center items-center h-screen'>
         <RouterProvider router={router} />
       </div>
